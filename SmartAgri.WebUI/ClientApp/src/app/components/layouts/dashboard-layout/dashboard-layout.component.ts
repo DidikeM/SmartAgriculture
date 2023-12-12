b52a6c0e66816aa9431aae1281469e60cea15006
burakduygun <duyqunburak@hotmail.com>
2023-12-12 19:22:28 +0300

    - [feature][dashboardlayout] : Added front-end for dashboard page.

--- SmartAgri.WebUI/ClientApp/src/app/components/layouts/dashboard-layout/dashboard-layout.component.ts
+++ SmartAgri.WebUI/ClientApp/src/app/components/layouts/dashboard-layout/dashboard-layout.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
 
+import { faBars, faMessage, faPlus, faRightFromBracket, faGear, faReceipt,faChartSimple, faUsers, faGauge } from '@fortawesome/free-solid-svg-icons';
+
 @Component({
   selector: 'app-dashboard-layout',
   templateUrl: './dashboard-layout.component.html',
   styleUrls: ['./dashboard-layout.component.css']
 })
 export class DashboardLayoutComponent {
+  faBars = faBars;
+  faMessage = faMessage;
+  faPlus = faPlus;
+  faRightFromBracket = faRightFromBracket;
+  faGear = faGear;
+  faReceipt = faReceipt;
+  faChartSimple = faChartSimple;
+  faUsers = faUsers;
+  faGauge = faGauge;
+  
+  public isNavigationActive = false;
 
+  public toggleNavigation(): void {
+    this.isNavigationActive = !this.isNavigationActive;
+  }
 }
